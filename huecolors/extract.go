ab51ae930d97bb93e8df786aa89631fc8f988e92
Hekmon <edouardhur@gmail.com>
2019-05-15 20:27:32 +0200

    extraction function done

---
+++ huecolors/extract.go
@@ -0,0 +1,60 @@
+package huecolors
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	_ "image/jpeg" // will register the jpg decode for image.Decode()
+	_ "image/png"  // will register the png decode for image.Decode()
+	"sort"
+
+	"github.com/EdlinOrg/prominentcolor"
+)
+
+var (
+	prominentcolorsParams = []int{
+		prominentcolor.ArgumentAverageMean | prominentcolor.ArgumentNoCropping,
+		prominentcolor.ArgumentAverageMean,
+		prominentcolor.ArgumentNoCropping,
+		prominentcolor.ArgumentDefault,
+	}
+)
+
+// CIExyY represents a color in the CIE xyY colorspace
+type CIExyY struct {
+	X         float64
+	Y         float64
+	Luminance float64
+}
+
+// GetHueColors returns the x (nbColors) main colors from imgData (jpg or png)
+func GetHueColors(nbColors int, imgData []byte) (colors []CIExyY, err error) {
+	// Decode data as image
+	img, _, err := image.Decode(bytes.NewBuffer(imgData))
+	if err != nil {
+		err = fmt.Errorf("can't decode data as image: %v", err)
+		return
+	}
+	// Create each set
+	var RGBcolors []prominentcolor.ColorItem
+	possibilities := make(ColorSets, len(prominentcolorsParams))
+	for index, params := range prominentcolorsParams {
+		// Extract main colors with current params
+		RGBcolors, err = prominentcolor.KmeansWithAll(nbColors, img, params,
+			prominentcolor.DefaultSize, prominentcolor.GetDefaultMasks())
+		if err != nil {
+			err = fmt.Errorf("prominent colors extraction failed with params '%s': %v", genPColorParamsString(params), err)
+			return
+		}
+		// Add them to the list
+		possibilities[index] = NewColorSet(RGBcolors, params)
+	}
+	// Get the set with the most differents colors
+	sort.Sort(sort.Reverse(possibilities))
+	// Build the answer
+	colors = make([]CIExyY, len(possibilities))
+	for index, color := range possibilities[0].GetColorfullSet() {
+		colors[index].X, colors[index].Y, colors[index].Luminance = color.Xyy()
+	}
+	return
+}
